79f0eaa5b2eb5707a3d1d8cf89a385e30ee21085
Matheus Henrique Caiser Barrozo <matheus.barrozo@qualirede.com.br>
2020-11-20 19:14:20 -0300

    DIRTY FALSE

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,13 +2,26 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "b8b72b1-dirty",
-    "hash": "b8b72b1",
-    "distance": null,
-    "tag": null,
-    "semver": null,
-    "suffix": "b8b72b1-dirty",
-    "semverString": null,
-    "version": "0.0.1"
+    "raw": "v0.0.1-0-g9f4b93b-dirty",
+    "hash": "g9f4b93b",
+    "distance": 0,
+    "tag": "v0.0.1",
+    "semver": {
+        "options": {
+            "loose": false,
+            "includePrerelease": false
+        },
+        "loose": false,
+        "raw": "v0.0.1",
+        "major": 0,
+        "minor": 0,
+        "patch": 1,
+        "prerelease": [],
+        "build": [],
+        "version": "0.0.1"
+    },
+    "suffix": "0-g9f4b93b-dirty",
+    "semverString": "0.0.1",
+    "version": "0.0.2"
 };
 /* tslint:enable */
