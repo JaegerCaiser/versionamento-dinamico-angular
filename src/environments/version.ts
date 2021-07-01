1e289031fdfc50648cfaeb8bcf6f87ced8ff6fd4
Matheus Caiser <matheus.barrozo@qualirede.com.br>
2021-07-01 11:44:59 -0300

    Testando avanço de versao

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,26 +2,26 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v0.0.2-0-g8b2b135-dirty",
-    "hash": "g8b2b135",
+    "raw": "v0.0.3-0-g829646f-dirty",
+    "hash": "g829646f",
     "distance": 0,
-    "tag": "v0.0.2",
+    "tag": "v0.0.3",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v0.0.2",
+        "raw": "v0.0.3",
         "major": 0,
         "minor": 0,
-        "patch": 2,
+        "patch": 3,
         "prerelease": [],
         "build": [],
-        "version": "0.0.2"
+        "version": "0.0.3"
     },
-    "suffix": "0-g8b2b135-dirty",
-    "semverString": "0.0.2",
+    "suffix": "0-g829646f-dirty",
+    "semverString": "0.0.3",
     "version": "0.0.2"
 };
 /* tslint:enable */
