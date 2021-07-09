1f66fe40813256b20d71a6d6c110b9095bacb7a5
Matheus Caiser <matheus.barrozo@qualirede.com.br>
2021-07-09 11:12:28 -0300

    Finalizado

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,26 +2,26 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v1.0.0-0-g1e28903-dirty",
-    "hash": "g1e28903",
+    "raw": "v1.0.1-0-gfb0ce56-dirty",
+    "hash": "gfb0ce56",
     "distance": 0,
-    "tag": "v1.0.0",
+    "tag": "v1.0.1",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.0.0",
+        "raw": "v1.0.1",
         "major": 1,
         "minor": 0,
-        "patch": 0,
+        "patch": 1,
         "prerelease": [],
         "build": [],
-        "version": "1.0.0"
+        "version": "1.0.1"
     },
-    "suffix": "0-g1e28903-dirty",
-    "semverString": "1.0.0",
+    "suffix": "0-gfb0ce56-dirty",
+    "semverString": "1.0.1",
     "version": "0.0.2"
 };
 /* tslint:enable */
